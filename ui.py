ceffad0972558e793cba325d581fd068d84dfe8d
cerik05 <107956477+cerik05@users.noreply.github.com>
2024-02-06 12:56:19 +0200

    Update ui.py

--- ui.py
+++ ui.py
@@ -5,59 +5,43 @@ import PySimpleGUI as sg
 class Ui():
     def __init__(self):
         self.service = Service()
-
     def run(self):
         main_layout = [
             [sg.Text("Choose user ")],
             [sg.Button("Applicant", key="1"), sg.Button("Admin", key="2"), sg.Button("Exit", key="0")]
-
         ]
-
-
         applicant_layout = [
             [sg.Text("Choose command:")],
             [sg.Listbox(
                 values=["1. Job list", "2. Apply for a job", "3. Highest paying jobs with lowest year requirement",
-                        "0. Exit"], size=(40, 4), key="-LIST-")],
+                        "0. Exit"], size=(40, 4), key="-LIST-", enable_events=True)],
             [sg.Button("OK"), sg.Button("Back")],
             [sg.Output(size=(40, 10))]
         ]
-
-
         admin_layout = [
             [sg.Text("Choose command:")],
             [sg.Listbox(values=["1. New job", "2. Remove a job", "3. Job list", "4. Inspecting a specific department",
-                                "5. Applicants in a specific job", "0. Exit"], size=(40, 6), key="-LIST-")],
+                                "5. Applicants in a specific job", "0. Exit"], size=(40, 6), key="-LIST-", enable_events=True)],
             [sg.Button("OK"), sg.Button("Back")],
             [sg.Output(size=(40, 10))]
         ]
-
         main_window = sg.Window("Main Window", main_layout, finalize=True)
         applicant_window = sg.Window("Applicant Window", applicant_layout, finalize=True)
         admin_window = sg.Window("Admin Window", admin_layout, finalize=True)
-
-
         applicant_window.hide()
         admin_window.hide()
-
-
         while True:
-            event, values = main_window.read()
-
+            event, values = main_window.read(timeout=100)
             if event in (None, "0", "Exit"):
                 break
-
             elif event == "1":
                 applicant_window.un_hide()
                 main_window.hide()
-
                 while True:
-                    event, values = applicant_window.read()
-
+                    event, values = applicant_window.read(timeout=100)
                     if event in (None, "0", "Exit", "Back"):
                         break
-
-                    elif event == "OK":
+                    elif event == "-LIST-":
                         command = values["-LIST-"][0][0]
                         if command == "1":
                             for job in self.service.getjoblist():
@@ -71,21 +55,16 @@ class Ui():
                             for job in self.service.getjoblist():
                                 if job.get_years() < years:
                                     print(job)
-
                 applicant_window.hide()
                 main_window.un_hide()
-
             elif event == "2":
                 admin_window.un_hide()
                 main_window.hide()
-
                 while True:
-                    event, values = admin_window.read()
-
+                    event, values = admin_window.read(timeout=100)
                     if event in (None, "0", "Exit", "Back"):
                         break
-
-                    elif event == "OK":
+                    elif event == "-LIST-":
                         command = values["-LIST-"][0][0]
                         if command == "1":
                             self.service.add_job()
@@ -95,18 +74,15 @@ class Ui():
                             for job in self.service.getjoblist():
                                 print(str(job))
                         elif command == "4":
-                            self.service.specific_jobs()
+                            for job in self.service.specific_jobs():
+                                print(str(job))
                         elif command == "5":
-                            self.service.job_applicants()
-
+                            for applicant in self.service.job_applicants():
+                                print(str(applicant))
                 admin_window.hide()
                 main_window.un_hide()
-
-
         main_window.close()
         applicant_window.close()
         admin_window.close()
-
-
 new_ui = Ui()
 new_ui.run()
